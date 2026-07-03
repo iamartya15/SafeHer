066b96142ea8f15a0c8c64bf8470acfc29328a12
Amartya Kushwaha <amartyakushwaha30@gmail.com>
2026-07-03 22:54:45 +0530

    cors fix32

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ const errorHandler = require('./middlewares/errorMiddleware');
 connectDB();
 
 const app = express();
+app.set("trust proxy", 1);
 
 // Set security headers with Helmet
 app.use(
@@ -49,7 +50,8 @@ app.use(
     origin: process.env.FRONTEND_URL || 'http://localhost:5173'
     ,
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
   })
 );
 
